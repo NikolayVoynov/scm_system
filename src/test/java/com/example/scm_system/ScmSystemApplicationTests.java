7a27678544220ec76c372baaf52afcaa5a5acae4
Nikolay Voynov <nikolay_voynov@abv.bg>
2021-11-07 11:06:45 +0200

    Initial commit

---
+++ src/test/java/com/example/scm_system/ScmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scm_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
