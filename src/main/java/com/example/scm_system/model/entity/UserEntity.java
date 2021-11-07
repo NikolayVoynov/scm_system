dc195371dcc53191f3e2b4ad68334e9f963a3db7
Nikolay Voynov <nikolay_voynov@abv.bg>
2021-11-07 15:09:47 +0200

    BaseEntity commit

---
+++ src/main/java/com/example/scm_system/model/entity/UserEntity.java
@@ -0,0 +1,9 @@
+package com.example.scm_system.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity{
+}
